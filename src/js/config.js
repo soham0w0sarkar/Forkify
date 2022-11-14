6ac8d2d35220a6752e12c0ffa5681994c083195f
Soham Sarkar <sohamsarkar59@gmail.com>
2022-11-15 01:41:48 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c245ac6d-ab14-4a12-8897-e5ab7898de71';
+export const MODAL_CLOSE_SEC = 2.5;
